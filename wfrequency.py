51dd76623fc29478da1b887a3332cf2f39398cfa
Harsha <harshayt8@gmail.com>
2026-07-20 20:28:07 +0530

    programs that count the frequnecy of each word in a sentence

---
+++ wfrequency.py
@@ -0,0 +1,15 @@
+#count the word frequency 
+
+def wfreq(text):
+    words=text.split()
+    frequency={}
+
+    for word in words:
+        if word in frequency:
+            frequency[word]+=1
+        else:
+            frequency[word]=1
+    return frequency
+
+text="data science data analytics"
+print(wfreq(text))
