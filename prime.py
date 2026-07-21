c35128e35b1eb5d27d29ed5d9af4535f8e37dc88
Harsha <harshayt8@gmail.com>
2026-07-21 17:18:14 +0530

    prime numbers from 1 to n range

--- prime.py
+++ prime.py
@@ -1,1 +1,21 @@
 #3) Write a Python program to print all prime numbers between 1 and 100.
+
+
+for num in range(2, 101):
+    prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            prime = False
+            break
+    if prime:
+        print(num)
+
+
+#or
+
+for num in range(2,101):
+    for i in range(2,num):
+        if num%i==0:
+            break
+    else:
+        print(num)
