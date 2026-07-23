3fb45658cd33661fee60d3f03adb08da8fca5499
Harsha <harshayt8@gmail.com>
2026-07-23 22:15:40 +0530

    longest word in a sentance

---
+++ longword.py
@@ -0,0 +1,19 @@
+#7. Find the longest word in a sentence.
+def long_word(sentence):
+    words=sentence.split()
+    return max(words, key=len)
+
+sentence=input("Enter a sentences: ")
+print(long_word(sentence))
+
+#or
+
+def long_word(sentence):
+    words = sentence.split()
+    longest = words[0]
+    for word in words:
+        if len(word) > len(longest):
+            long = word
+    return long
+sentence = input("Enter a sentence: ")
+print("Longest word:", long_word(sentence))
