f225c112ee854be118361b2a75fa2c3e67667520
Harsha <harshayt8@gmail.com>
2026-07-28 13:10:03 +0530

    Student class with methods to calculate the average marks and grade.

---
+++ marksgrade.py
@@ -0,0 +1,31 @@
+#2.Create a Student class with methods to calculate the average marks and grade.
+class student:
+    def __init__(self,name,science,mathematics,English,Grammer):
+        self.name=name
+        self.science=science
+        self.mathematics=mathematics
+        self.English=English
+        self.Grammer=Grammer
+
+    def calculate_avg(self):
+        avg=(self.science+self.mathematics+self.English+self.Grammer)/3
+        return avg
+    def calculate_grade(self):
+        avg=self.calculate_avg()
+        if avg>=90:
+            return "A"
+        elif avg>=75:
+            return "B"
+        elif avg>=55:
+            return "C"
+        else:
+            return"Failed..!"
+
+#creating an object
+s1=student("Harsha",90,41,63,50)
+
+#Display the details
+print("student Name:",s1.name)
+print("Marks:",s1.calculate_avg())
+print("Grade:",s1.calculate_grade())
+        
