8c64b987fd2d133789e79a37669b1527477a7b0a
Harsha <harshayt8@gmail.com>
2026-08-02 22:42:16 +0530

    even using lambda

--- lambda.py
+++ lambda.py
@@ -3,7 +3,12 @@ square=lambda x:x**2
 a=int(input("Enter a number to be squared: "))
 print(square(a))
 
-#Create a lambda function to add two numbers.
+# #Create a lambda function to add two numbers.
 sum=lambda x,y:x+y
 result=sum(3,5)
 print(result)
+
+#Create a lambda function to check if a number is even.
+even=lambda x:"even" if x%2==0 else "odd"
+a=even(3)
+print(a)
