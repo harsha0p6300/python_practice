f979e4207b7ab14d84461bcca2aa39c0c5ed8ff2
Harsha <harshayt8@gmail.com>
2026-08-02 22:27:57 +0530

    lambda functions

---
+++ lambda.py
@@ -0,0 +1,9 @@
+#Create a lambda function to square a number.
+square=lambda x:x**2
+a=int(input("Enter a number to be squared: "))
+print(square(a))
+
+#Create a lambda function to add two numbers.
+sum=lambda x,y:x+y
+result=sum(3,5)
+print(result)
