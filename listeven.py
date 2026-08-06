9b576700ced9bde4019d560aaff7fea538644524
Harsha <harshayt8@gmail.com>
2026-08-06 22:27:19 +0530

    adding files

--- listeven.py
+++ listeven.py
@@ -2,3 +2,10 @@
 list1=[12, 15, 18, 21, 24, 27]
 even=filter(lambda x:x%2,list1)
 print(list(even))
+
+
+#by using the function
+def even_list(list2):
+    return list(filter(lambda x:x%2,list2))
+
+print(even_list([12, 15, 18, 21, 24, 27]))
